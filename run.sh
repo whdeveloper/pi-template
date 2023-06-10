080261c1f8791593910103af6128f86923a5bd16
whdeveloper <whdeveloper@gmail.com>
2023-06-10 23:09:11 +0200

    Updated run.sh with environment variable, replaced the volume mount (as the ansible image doesn't output something itself)

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker network create raspi_network
 docker build -t ansible-pi-template .
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ./dist:/dist --network raspi_network ansible-pi-template
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e DIST_DIR=./dist --network raspi_network ansible-pi-template
 docker network remove raspi_network
